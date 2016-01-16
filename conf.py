805b575e75e69d74da2e6395b44af652e6e00ce4
bast <bast@users.noreply.github.com>
2016-01-16 20:46:56 +0100

    s/2014/2016/

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Software development toolbox'
-copyright = u'2014, Radovan Bast'
+copyright = u'2016, Radovan Bast'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
